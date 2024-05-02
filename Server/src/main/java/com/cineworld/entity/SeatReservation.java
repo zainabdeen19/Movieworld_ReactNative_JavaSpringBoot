db74f65b5484c87990d8a2434ef4cd60f9081271
Zain Abdeen <zainabiddeen19@gmail.com>
2024-05-02 00:00:00 -0400

    Commit: SeatReservation.java

---
+++ Server/src/main/java/com/cineworld/entity/SeatReservation.java
@@ -0,0 +1,31 @@
+package com.movieworld.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "seat_reservations")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SeatReservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "seat_id", nullable = false)
+    private Seat seat;
+
+    @ManyToOne
+    @JoinColumn(name = "showtime_id", nullable = false)
+    private Showtime showtime;
+
+
+
+
+}
+ 
+ 
