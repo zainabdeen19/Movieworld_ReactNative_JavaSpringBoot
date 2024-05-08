625022c5c59672fc26f3e01db9c0557521e264fe
Zain Abdeen <zainabiddeen19@gmail.com>
2024-05-08 00:00:00 -0400

    Commit: style.js

---
+++ Client/style/style.js
@@ -0,0 +1,10 @@
+// you can change these colors to change the look of the app ;)
+export const theme = {
+    background: '#cca216',
+    text: '#cca216'
+ }
+ export const generalStyles = {
+    text: {color: theme.text},
+    background: {backgroundColor: theme.background}
+ } 
+ 
