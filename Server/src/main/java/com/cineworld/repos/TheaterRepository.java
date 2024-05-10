f8763965e858fbb5480407cf732f105c98040e49
Zain Abdeen <zainabiddeen19@gmail.com>
2024-05-10 00:00:00 -0400

    Commit: TheaterRepository.java

---
+++ Server/src/main/java/com/cineworld/repos/TheaterRepository.java
@@ -0,0 +1,9 @@
+package com.movieworld.repos;
+
+import com.movieworld.entity.Theater;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TheaterRepository extends JpaRepository<Theater, Long> {
+
+}
+ 
