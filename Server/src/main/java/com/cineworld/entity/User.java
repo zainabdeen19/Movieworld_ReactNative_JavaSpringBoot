ff89859dff20bd947f7796e2570bbebb997c6870
Zain Abdeen <zainabiddeen19@gmail.com>
2024-05-17 00:00:00 -0400

    Commit: User.java

---
+++ Server/src/main/java/com/cineworld/entity/User.java
@@ -0,0 +1,30 @@
+package com.movieworld.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    private String email;
+    private String fullName;
+    private String phoneNumber;
+
+
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Booking> bookings;
+}
+ 
+ 
