2354419e2b41efaa29decc7a93c4d792b94bbeec
Zain Abdeen <zainabiddeen19@gmail.com>
2024-05-20 00:00:00 -0400

    Commit: MovieService.java

---
+++ Server/src/main/java/com/cineworld/services/MovieService.java
@@ -0,0 +1,47 @@
+package com.movieworld.services;
+
+import com.movieworld.entity.Movie;
+import com.movieworld.repos.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MovieService {
+
+    @Autowired
+    private MovieRepository movieRepository;
+
+    // Create
+    public Movie createMovie(Movie movie) {
+        return movieRepository.save(movie);
+    }
+
+    // Read
+    public Movie getMovieById(Long id) {
+        return movieRepository.findById(id).orElse(null);
+    }
+
+    public List<Movie> getAllMovies() {
+        return movieRepository.findAll();
+    }
+
+    // Update
+    public Movie updateMovie(Long id, Movie movieDetails) {
+        Movie movie = movieRepository.findById(id).orElse(null);
+        if (movie != null) {
+            movie.setTitle(movieDetails.getTitle());
+            // set other fields
+            movieRepository.save(movie);
+        }
+        return movie;
+    }
+
+    // Delete
+    public void deleteMovie(Long id) {
+        movieRepository.deleteById(id);
+    }
+}
+
+ 
