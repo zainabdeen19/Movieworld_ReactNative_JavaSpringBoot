cf067437f43d611ffc4eeffbe18248858fc4e60b
Zain Abdeen <zainabiddeen19@gmail.com>
2024-06-11 00:00:00 -0400

    Commit: Seat.java

---
+++ Server/src/main/java/com/cineworld/entity/Seat.java
@@ -0,0 +1,29 @@
+package com.movieworld.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "seats")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seat {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Character rowChar;
+
+    @ManyToOne
+    @JoinColumn(name = "screen_id", nullable = false)
+    @JsonIgnore
+    private Screen screen;
+
+    private String seatNumber;
+
+}
+ 
+ 
