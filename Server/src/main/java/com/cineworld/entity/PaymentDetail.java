752ffbe6ed7fb3ee65ebf9711475fe6fd97b7707
Zain Abdeen <zainabiddeen19@gmail.com>
2024-06-16 00:00:00 -0400

    Commit: PaymentDetail.java

---
+++ Server/src/main/java/com/cineworld/entity/PaymentDetail.java
@@ -0,0 +1,31 @@
+package com.movieworld.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payment_details")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class PaymentDetail {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentDetailId;
+
+    private String cardNumber;
+
+    private LocalDate expirationDate;
+    private String cardHolderName;
+    private String cvv;
+
+    @OneToOne(mappedBy = "paymentDetail")
+    @JsonIgnore
+    private Booking booking;
+}
+
+ 
+ 
