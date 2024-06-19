03ddb754b8e1cec166a3670a877c426ae1bb10f6
Zain Abdeen <zainabiddeen19@gmail.com>
2024-06-19 00:00:00 -0400

    Commit: UserDTO.java

---
+++ Server/src/main/java/com/cineworld/DTO/UserDTO.java
@@ -0,0 +1,16 @@
+package com.movieworld.DTO;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+
+    private String fullName;
+
+    private String email;
+
+    private String phoneNumber;
+
+}
+ 
+ 
