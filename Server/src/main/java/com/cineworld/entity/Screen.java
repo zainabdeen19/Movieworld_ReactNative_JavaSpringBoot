c9705790efee1737a8a91633d8cb9cbe0e4bbd46
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-01 00:00:00 -0400

    Commit: Screen.java

---
+++ Server/src/main/java/com/cineworld/entity/Screen.java
@@ -0,0 +1,38 @@
+package com.movieworld.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+
+@Entity
+@Table(name = "screens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Screen {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int screenNumber;
+
+
+
+
+
+    @ManyToOne
+    @JoinColumn(name = "theater_id", nullable = false)
+    @JsonIgnore
+    private Theater theater;
+
+    @OneToMany(mappedBy = "screen", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Showtime> showtimes;
+}
+ 
+ 
