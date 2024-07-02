fac265b41815289a4be7c17502936e51ba8e6746
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-02 00:00:00 -0400

    Commit: PaymentRequest.java

---
+++ Server/src/main/java/com/cineworld/DTO/PaymentRequest.java
@@ -0,0 +1,14 @@
+package com.movieworld.DTO;
+
+import lombok.Data;
+
+@Data
+public class PaymentRequest {
+
+        private PaymentDetailDTO paymentDetail;
+        private UserDTO user;
+
+
+
+}
+ 
