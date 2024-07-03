36035b5748276c36441f6c883b56cd78ebe9cfca
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-03 00:00:00 -0400

    Commit: ScreenRepository.java

---
+++ Server/src/main/java/com/cineworld/repos/ScreenRepository.java
@@ -0,0 +1,8 @@
+package com.movieworld.repos;
+
+import com.movieworld.entity.Screen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
+}
+ 
