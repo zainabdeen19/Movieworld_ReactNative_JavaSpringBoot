5810c93277d9a62fdac86f774aa6f056a88cf743
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-03 00:00:00 -0400

    Commit: CineworldTicketAppBeSpringApplication.java

---
+++ Server/src/main/java/com/cineworld/CineworldTicketAppBeSpringApplication.java
@@ -0,0 +1,14 @@
+package com.movieworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class movieworldTicketAppBeSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(movieworldTicketAppBeSpringApplication.class, args);
+    }
+
+}
+ 
