589b8e73f3e59863ab0a450387bda49c61d56a60
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-09 00:00:00 -0400

    Commit: CineworldTicketAppBeSpringApplicationTests.java

---
+++ Server/src/test/java/com/cineworld/CineworldTicketAppBeSpringApplicationTests.java
@@ -0,0 +1,14 @@
+package com.movieworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class movieworldTicketAppBeSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+ 
