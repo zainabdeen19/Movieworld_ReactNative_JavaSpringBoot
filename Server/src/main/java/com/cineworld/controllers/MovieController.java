9cf556551c16bf320edb0c545ba331a9b92066e1
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-19 00:00:00 -0400

    Commit: MovieController.java

---
+++ Server/src/main/java/com/cineworld/controllers/MovieController.java
@@ -0,0 +1,66 @@
+package com.movieworld.controllers;
+
+import com.movieworld.entity.Movie;
+import com.movieworld.services.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/movies")
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+    // Create a new movie
+    @PostMapping("/")
+    public ResponseEntity<Movie> createMovie(@RequestBody Movie movie) {
+        Movie newMovie = movieService.createMovie(movie);
+        return ResponseEntity.ok(newMovie);
+    }
+
+    // Get a single movie by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Movie> getMovieById(@PathVariable Long id) {
+        Movie movie = movieService.getMovieById(id);
+        if (movie == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(movie);
+    }
+
+    // Get all movies
+    @GetMapping("/")
+    public List<Movie> getAllMovies() {
+        return movieService.getAllMovies();
+    }
+
+    // Update a movie
+    @PutMapping("/{id}")
+    public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody Movie movieDetails) {
+        try {
+            Movie updatedMovie = movieService.updateMovie(id, movieDetails);
+            return ResponseEntity.ok(updatedMovie);
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    // Delete a movie
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMovie(@PathVariable Long id) {
+        try {
+            movieService.deleteMovie(id);
+            return ResponseEntity.ok().build();
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+}
+
+ 
+ 
