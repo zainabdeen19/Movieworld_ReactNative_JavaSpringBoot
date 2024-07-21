b8354965ddfc3e9de10837d0930bb2fe0ebef009
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-21 00:00:00 -0400

    Commit: Theater.java

---
+++ Server/src/main/java/com/cineworld/entity/Theater.java
@@ -0,0 +1,33 @@
+package com.movieworld.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "theaters")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Theater {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String location;
+    private int numberOfScreens;
+
+    @OneToMany(mappedBy = "theater", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Screen> screens;
+
+    @OneToMany(mappedBy = "theater", cascade = CascadeType.ALL)
+    @JsonIgnore
+    private List<Showtime> showtimes;
+}
+
+ 
+ 
