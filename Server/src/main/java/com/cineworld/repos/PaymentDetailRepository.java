966c253cc4200c008caae1cc2512b2dd892a2324
Zain Abdeen <zainabiddeen19@gmail.com>
2024-07-28 00:00:00 -0400

    Commit: PaymentDetailRepository.java

---
+++ Server/src/main/java/com/cineworld/repos/PaymentDetailRepository.java
@@ -0,0 +1,9 @@
+package com.movieworld.repos;
+
+import com.movieworld.entity.PaymentDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PaymentDetailRepository extends JpaRepository<PaymentDetail, Long> {
+}
+ 
+ 
